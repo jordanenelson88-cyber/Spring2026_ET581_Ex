2373653ea294ff73bde9801db0dc71fc21091250
jordanenelson88-cyber <jordanenelson88@gmail.com>
2026-03-05 06:54:02 +0000

    commit

--- Homework/SeparateEvenAndOddArrays.java
+++ Homework/SeparateEvenAndOddArrays.java
@@ -2,11 +2,13 @@ import java.util.Arrays;
 
 public class SeparateEvenAndOddArrays{
     public static void main(String [] args){
-        int [] nums = {3, 5, 8, 12, 7, 4};
+        int [] nums = {3, 5, 8, 12, 7, 4}; // now create the original array of numbers
 
+        // these will be the varibles use to count how many even and odd numbers exist.
         int  evenCount = 0;
         int oddCount = 0;
 
+        // loop runs through the array to count even and odd numbers.
         for( int num: nums){
             if(num % 2 == 0){
                 evenCount++;
@@ -16,13 +18,18 @@ public class SeparateEvenAndOddArrays{
             
         
         }
+
+        // create a array that will hold the exact size of the even and odd numbers
         int [] evenArray = new int[evenCount];
         int [] oddArray = new int[oddCount];
 
+
+        // the index varibles will track where to place numbers in each new array
         int evenIndex = 0;
         int oddIndex = 0;
 
 
+        // now you'll loop through the original array again to place numbers into the correct array
         for(int num : nums){
             if(num % 2 == 0){
             evenArray[evenIndex] = num;
@@ -32,6 +39,7 @@ public class SeparateEvenAndOddArrays{
             oddIndex++;
         }
         }
+        // you should now be able to print the original array and the results.
         System.out.println("Original array: " + Arrays.toString(nums));
         System.out.println("Even number count: " + evenCount);
         System.out.println("Odd number count: " + oddCount);
--- Homework/SumAverageMinimumAndMaximum.java
+++ Homework/SumAverageMinimumAndMaximum.java
@@ -5,43 +5,48 @@ public class SumAverageMinimumAndMaximum {
 
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("Enter the number of elements: ");
+        Scanner input = new Scanner(System.in); // create a scanner that could reaf number inputed from the user 
+        
+        System.out.println("Enter the number of elements: "); // ask the user how many numbers they would like to enter in so we will know the size of the array
         int num = input.nextInt();
 
-        int [] numbers = new int[num];
+        int [] numbers = new int[num]; // Create an integer array that will store all the numbers the user enters.
 
-        System.out.println("Enter "   + num +  "integers:" );
+        System.out.println("Enter "   + num +  "integers:" ); // user will need to input the numbers that will be stored in the array.
 
-        for (int i = 0 ; i < num ; i+=1) {
+        for (int i = 0 ; i < num ; i+=1) { // this loop will run num and read each number that will be stored into the array. 
             numbers[i] = input.nextInt();
         }
 
+        // Initialize the sum, min, and max values.
         int sum = 0;
         int min = numbers [0];
         int max = numbers [0];
 
-        for (int i = 0 ; i < num ; i++  ) {
+        for (int i = 0 ; i < num ; i++  ) {  // This loop will go through each  element in the array to calculate the sum and find the min and max values.
 
-            sum  += numbers[i];
+            sum  += numbers[i]; // You would then add the current number in the array to run the total of sum.
 
-            if(numbers[i] < min) {
+            if(numbers[i] < min) { // Check if the current number is smaller than the stored minimum and update it if necessary.
                 min = numbers[i];
             }
 
-            if (numbers[i] > max){
-                max = numbers[i];
-            }
+            if (numbers[i] > max){ // Check if the current number is larger than the stored maximum and update it if necessary.
+             max = numbers[i];
+            } 
         }
 
-        double average = (double) sum / num;
+        double average = (double) sum / num;  // nocw alculate the average by dividing the total sum by the number.
 
-        System.out.println("Sum = " + sum);
-        System.out.println("Average = "  + average);
-        System.out.println(" Minimum = " + min);
-        System.out.println("Maximum = " + max);
+        System.out.println("Sum = " + sum);  // will now show the total sum of number found in the array.
+        System.out.println("Average = "  + average);  // will now show the average of number found in the array.
+        System.out.println(" Minimum = " + min);  // will now show the smallest value found in the array.
+        System.out.println("Maximum = " + max);  // will now show the largest value found in the array.
 
-        input.close();
-    }
+        input.close(); //  now where gonna close the scanner 
+   
+    }   
+
+
+
 }
