be901b947e26576d83b6b227e77bd2e293246f3b
jordanenelson88-cyber <jordanenelson88@gmail.com>
2026-03-12 01:53:12 +0000

    commit

--- Homework/StringNestedLoopHomework.java
+++ Homework/StringNestedLoopHomework.java
@@ -1,51 +1,51 @@
 public class StringNestedLoopHomework {
     // Question 3
-    public static void reverseEachWord(String sentence) {
-        String [] words = sentence.split(" ");        
+    public static void reverseEachWord(String sentence) { // with this method we will reverse each word in the sentence
+        String [] words = sentence.split(" "); // sentence.split will split the sentences into words by using spaces        
         
-        for (int i = 0; i < words.length; i++){
+        for (int i = 0; i < words.length; i++){ // we will run the loop for each word in the array and words.length will tell us how many words are there
             
-            for (int j = words[i].length() - 1;j >= 0; j--){
-                System.out.println(words[i].charAt(j));
+            for (int j = words[i].length() - 1;j >= 0; j--){ // this loop will start at the last letter and then move backwards 
+                System.out.print(words[i].charAt(j)); // now this will print out each word but in reverse order
             }
-            System.out.println();
+            System.out.println(); // move onto the next line after finishing one word
         }
     }
-    // Question 4
-    public static void reverseWordOrder(String sentence) {
-        String [] words = sentence.split(" ");
 
-         for (int i = words.length - 1; i >= 0; i--) {
-            System.out.println(words[i]);
+    // Question 4
+    public static void reverseWordOrder(String sentence) { // This method reverses the order of the words in the sentence
+        String [] words = sentence.split(" "); // sentence.split will split the sentences into words by using spaces 
+
+         for (int i = words.length - 1; i >= 0; i--) { // this loop starts at the last word and moves backwards
+            System.out.println(words[i]); // now this will print out each word but in reverse order
         }
     }
 
     // Question 5
     public static void reverseLongWords(String sentence) {
 
-        String[] words = sentence.split(" ");
+        String[] words = sentence.split(" "); // This method reverses only if the word is larger than 3 letter
 
-        for (int i = 0; i < words.length; i++) {
+        for (int i = 0; i < words.length; i++) { // sentence.split will split the sentences into words by using spaces 
 
-            if (words[i].length() > 3) {
+            if (words[i].length() > 3) { // this will check if word has more than 3 letters
 
-                for (int j = words[i].length() - 1; j >= 0; j--) {
-                    System.out.print(words[i].charAt(j));
+                for (int j = words[i].length() - 1; j >= 0; j--) {  // this loop will start at the last letter and then move backwards 
+                    System.out.print(words[i].charAt(j)); // now this will print out each word but in reverse order
                 }
 
             } else {
-                System.out.print(words[i]);
+                System.out.print(words[i]);  // if the word is 3 letters or less it prints normally
             }
 
-            System.out.print(" ");
+            System.out.print(" "); // this will allow space after each word
         }
     }
 
     public static void main(String[] args) {
         
-        reverseEachWord("Hello World");
-        reverseWordOrder("Java is fun");
-        reverseLongWords("Java is very cool");
+        reverseEachWord("Hello World"); // this method will reverse each word
+        reverseWordOrder("Java is fun"); // this method will reverse the order of the words
+        reverseLongWords("Java is very cool"); // this method will reverse only the long words
     }
 }
-
--- Homework/TwoDArrayHomework.java
+++ Homework/TwoDArrayHomework.java
@@ -1,28 +1,29 @@
-
 public class TwoDArrayHomework {
-    
-    public static void sumOfRows(int[][] arr) {
+    // Question 1
+    public static void sumOfRows(int[][] arr) { // this method will find the sum of each row 
        
-        for (int i = 0; i< arr.length; i++) {
-            int sum = 0;
+        for (int i = 0; i< arr.length; i++) { // we will run the loop for each row in the array and arr.length will tell us how many rows are there
+            int sum = 0;  // store the total for the current row
 
-            for(int j = 0; j< arr[i].length; j++){
-                sum += arr[i][j];
+            for(int j = 0; j< arr[i].length; j++){ // we will run the loop for each row in the array and arr[i].length will tell us how many columns are in the row
+                sum += arr[i][j]; //  this adds each number in the row to sum 
             
             }
-            System.out.println("Sum of row " + i + " : " + sum);
+            System.out.println("Sum of row " + i + " : " + sum); // this will print the total sum of each row 
         }
      
      
     }
-    public static void sumOfColumns(int[][] arr) {
-        for(int j = 0; j < arr[0].length; j++){
-            int sum = 0;
+    
+    // Question 2
+    public static void sumOfColumns(int[][] arr) { // this method will find the sum of each column 
+        for(int j = 0; j < arr[0].length; j++){ // we will run the loop for each column in the array and arr[0].length will tell us how many columns are there
+            int sum = 0; // store the total for the current row
 
-            for(int i = 0; i < arr[0].length; i++){
-                sum += arr[i][j];
+            for(int i = 0; i < arr.length; i++){ // // we will run the loop for each column int the array and arr.length will tell us how many columns are in the row
+                sum += arr[i][j]; //  this adds each number in the row to sum 
             }
-            System.out.println("Sum of column" + j + ":" + sum);
+            System.out.println("Sum of column" + j + ":" + sum); // this will print the total sum of each column
 
 
         }
@@ -31,10 +32,10 @@ public class TwoDArrayHomework {
         int [][] arr = {
             {1,2,3,4},
             {5,6,7,8},
-            {8,9,10,11,12}
+            {9,10,11,12}
         };
-        sumOfRows(arr);
-        sumOfColumns(arr);
+        sumOfRows(arr); // now sumOfRow will find row sum
+        sumOfColumns(arr); // now sumOfColumns will find column sum
     }
     
 }   
